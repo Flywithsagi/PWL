67ebfa5ff113b49ad871dbb4c02714bf692b189f
Satriowish <satriowisnuap@gmail.com>
2025-03-08 22:33:44 +0700

    Views

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,11 +6,12 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    public function hello()
-    {
-        return 'Hello World';
-    }
-    public function greeting(){
-        return view('blog.hello', ['name' => 'Satrio']);
+    public function hello(){
+        return('Hello World');
         }
+       public function greeting(){
+        return view('blog.hello')
+        ->with('name','Satrio Wisnu Adi Pratama')
+        ->with('occupation','Punk');
+        }       
 }
