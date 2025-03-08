2926f2561813c9415fea33b5f2dc158765336aae
Satriowish <satriowisnuap@gmail.com>
2025-03-08 22:01:21 +0700

    Controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -51,6 +54,11 @@ Route::get('/user/{name?}', function ($name='satrio') {
     return 'Nama saya '.$name;
 });
 
-Route::get('/', [PageController::class,'index']);
-Route::get('about', [PageController::class,'about']);
-Route::get('/articles/{id}', [PageController::class,'articles']);
+// Route::get('/', [PageController::class,'index']);
+// Route::get('about', [PageController::class,'about']);
+// Route::get('/articles/{id}', [PageController::class,'articles']);
+
+
+Route::get('/', [HomeController::class,'index']);
+Route::get('about', [AboutController::class,'about']);
+Route::get('/articles/{id}', [ArticleController::class,'articles']);
