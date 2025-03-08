c18e56de3be4b8179bf1dfbc874090caf838ac92
Satriowish <satriowisnuap@gmail.com>
2025-03-08 22:25:34 +0700

    Views

--- routes/web.php
+++ routes/web.php
@@ -64,9 +64,16 @@ Route::get('about', [AboutController::class,'about']);
 Route::get('/articles/{id}', [ArticleController::class,'articles']);
 
 Route::resource('photos', PhotoController::class)->only([
-    'index', 'show'
-   ]);
-   Route::resource('photos', PhotoController::class)->except([
+    'index', 'show'   
+]);
+Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
-   ]);
+]);
+
+// Route::get('/greeting', function () {
+//     return view('hello', ['name' => 'Satrio Wisnu Ai Pratama']);
+// });
+  
+Route::get('/greeting', [WelcomeController::class,'greeting']);
+
    
